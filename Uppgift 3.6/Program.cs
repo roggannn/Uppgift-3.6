bf5fe488e14c917cc16ee54ec85b8b04e9b4ed24
Admin <Admin@TEB123003>
2023-10-20 15:17:07 +0200

    Add project files.

---
+++ Uppgift 3.6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Skriv in förnamn och efternamn på samma rad");
+
+            string namn = Console.ReadLine();
+            int mellanslagsindex = namn.IndexOf(" ");
+
+            string förnamn = namn[..mellanslagsindex];
+            string efternamn = namn[(mellanslagsindex + 1)..];
+
+            if (förnamn.CompareTo(efternamn) < 0)
+            {
+                Console.WriteLine("Ditt förnamn är först i bokstavsordning");
+            }
+            else if (förnamn.CompareTo(efternamn) > 0)
+            {
+                Console.WriteLine("Ditt efternamn är först i bokstavsordning");
+            }
+            else
+            {
+                Console.WriteLine("Du har samma förnamn som efternamn");
+            }
+
+        }
+
+    }
+}
